72f9c7b6e289a36716f5beb91ba66679260fc2a0
pazeik <hannick.abdul-kuthoos@ensg.eu>
2024-03-28 10:50:37 +0100

    added front end patching

--- patch/test_patch_front/main.js
+++ patch/test_patch_front/main.js
@@ -1,4 +1,8 @@
 import * as GeoTIFF from "geotiff";
+import JSZip from "jszip";
+import { saveAs } from 'file-saver';
+
+var zip = new JSZip();
 
 // Fonction pour charger le GeoTIFF à partir du fichier uploadé par l'utilisateur
 async function loadGeoTIFF(file) {
@@ -14,40 +18,45 @@ async function loadGeoTIFF(file) {
       // Lire une image spécifique du GeoTIFF (par exemple, l'image 0)
       const image = await tiff.getImage();
 
-      // Obtenir les métadonnées de l'image
-      const width = image.getWidth();
-      const height = image.getHeight();
-      const tileWidth = image.getTileWidth();
-      const tileHeight = image.getTileHeight();
-      const samplesPerPixel = image.getSamplesPerPixel();
+      const pool = new GeoTIFF.Pool();
 
-      // when we are actually dealing with geo-data the following methods return
-      // meaningful results:
-      const origin = image.getOrigin();
-      const resolution = image.getResolution();
-      const bbox = image.getBoundingBox();
+      const patchSizeSelect = document.getElementById('patchSize');
 
-      console.log('width:', width, 'height:', height, 'tileWidth:', tileWidth, 'tileHeight:', tileHeight, 'samplesPerPixel:', samplesPerPixel);
-      console.log('origin:', origin, 'resolution:', resolution, 'bbox:', bbox);
+      let patchSize = parseInt(patchSizeSelect.value);
 
-      const patchSize = 256;
+      patchSizeSelect.onchange = function () {
+        patchSize = parseInt(this.value);
+      }
+
       const patches = [];
-      for (let i = 0; i < width; i += patchSize) {
-        for (let j = 0; j < height; j += patchSize) {
+      for (let i = 0; i < image.getWidth(); i += patchSize) {
+        for (let j = 0; j < image.getHeight(); j += patchSize) {
           const patch = await image.readRasters({
+            pool: pool,
             window: [i, j, i + patchSize, j + patchSize],
+            interleave: true,
+          });
+          getImageMetadata(image, i, j, 512).then(metadata => {
+            const arrayBufferTest = GeoTIFF.writeArrayBuffer(patch, metadata);
+            const blob = new Blob([arrayBufferTest], { type: 'application/octet-stream' });
+
+            zip.file(`image_${i}_${j}.tiff`, blob);
           });
           patches.push(patch);
         }
       }
 
-      // Show the download button once the GeoTIFF is loaded
+      console.log(patches.length)
+
       const downloadBtn = document.getElementById('downloadBtn');
       downloadBtn.style.display = 'block';
 
-      // Add click event to the download button
       downloadBtn.addEventListener('click', function () {
-        downloadGeoTIFF(arrayBuffer.slice(0, 256 * 256 * 3), 'image.tiff');
+        zip.generateAsync({ type: "blob" }).then(function (content) {
+          // see FileSaver.js
+          saveAs(content, "example.zip");
+        });
+
       });
 
     };
@@ -57,6 +66,42 @@ async function loadGeoTIFF(file) {
   } catch (error) {
     console.error('Erreur lors du chargement du GeoTIFF : ', error);
   }
+}
+
+// Fonction pour obtenir les métadonnées de l'image
+async function getImageMetadata(image, patchIndexX, patchIndexY, patchSize) {
+  const fileDir = image.getFileDirectory();
+  const ModelPixelScale = fileDir.ModelPixelScale;
+  const ModelTiepoint = fileDir.ModelTiepoint;
+  const GeoKeyDirectory = image.fileDirectory.GeoKeyDirectory;
+  const BitsPerSample = fileDir.BitsPerSample;
+  const GeoAsciiParams = fileDir.GeoAsciiParams;
+  const PhotometricInterpretation = fileDir.PhotometricInterpretation;
+  const Compression = fileDir.Compression;
+  const SamplesPerPixel = fileDir.SamplesPerPixel;
+  const Orientation = 1;
+  const ProjectedCSTypeGeoKey = image.geoKeys.ProjectedCSTypeGeoKey;
+
+  return {
+    height: patchSize,
+    width: patchSize,
+    GeoAsciiParams: GeoAsciiParams,
+    BitsPerSample: BitsPerSample,
+    SamplesPerPixel: SamplesPerPixel,
+    PhotometricInterpretation: PhotometricInterpretation,
+    Compression: Compression,
+    Orientation: Orientation,
+    ProjectedCSTypeGeoKey: ProjectedCSTypeGeoKey,
+    ModelTiepoint: [
+      0, 0, 0,
+      ModelTiepoint[3] + (patchIndexX * ModelPixelScale[0]),
+      ModelTiepoint[4] - (patchIndexY * ModelPixelScale[1]),
+      ModelTiepoint[5]
+    ],
+    ModelPixelScale: ModelPixelScale,
+    GeoKeyDirectory: GeoKeyDirectory
+  };
+
 }
 
 // Function to trigger download of the GeoTIFF
