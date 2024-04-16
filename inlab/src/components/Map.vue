d690108bdb9993ebc49e7aef1c5f1b90f6d79796
Cirederf <frederic.ye@ensg.eu>
2024-04-16 11:21:19 +0200

    FUSION

--- inlab/src/components/Map.vue
+++ inlab/src/components/Map.vue
@@ -1,57 +1,80 @@
 <template>
-    <div class="container-map">
-        <div id="map"></div>
-    </div>
+  <div class="container">
+    <stac-file-manager class="stacReader" />
+    <div id="map"></div>
+  </div>
 </template>
 
+<style scoped>
+.container {
+  display: flex;
+}
+
+.stacReader {
+  width: 25%;
+  height: 100vh;
+  overflow: auto;
+  border-right: 1px solid #ccc;
+}
+
+#map {
+  width: 75%;
+  height: 100vh;
+}
+</style>
+
 <script>
-import { Map, View } from 'ol';
-import { Tile as TileLayer } from 'ol/layer';
-import { OSM } from 'ol/source';
-import STACLayer from 'ol-stac';
-import proj4 from 'proj4';
-import { register } from 'ol/proj/proj4.js';
+import { Map, View } from "ol";
+import { Tile as TileLayer } from "ol/layer";
+import { OSM } from "ol/source";
+import STACLayer from "ol-stac";
+import proj4 from "proj4";
+import { register } from "ol/proj/proj4.js";
+import StacFileManager from "../components/StacFileManager.vue";
 
 register(proj4);
 
 export default {
-    name: 'Map',
-    mounted() {
-        const stac = new STACLayer({
-            url: 'https://s3.us-west-2.amazonaws.com/sentinel-cogs/sentinel-s2-l2a-cogs/10/T/ES/2022/7/S2A_10TES_20220726_0_L2A/S2A_10TES_20220726_0_L2A.json',
-        });
+  components: {
+    StacFileManager,
+  },
+  data() {
+    return {
+      map: null,
+    };
+  },
+  provide() {
+    return {
+      map: this.$data, // Provide the entire data object
+    };
+  },
+  name: "Map",
+  mounted() {
+    const stac = new STACLayer({
+      url: "https://s3.us-west-2.amazonaws.com/sentinel-cogs/sentinel-s2-l2a-cogs/10/T/ES/2022/7/S2A_10TES_20220726_0_L2A/S2A_10TES_20220726_0_L2A.json",
+    });
 
-        this.map = new Map({
-            target: 'map', // 'map' corresponds to the ID of the element
-            layers: [
-                new TileLayer({
-                    source: new OSM()
-                }),
-                stac
-            ],
-            view: new View({
-                center: [0, 0],
-                zoom: 2
-            }),
-            moveTolerance: 5
-        });
+    this.map = new Map({
+      target: "map",
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+        stac,
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
 
-        stac.on('sourceready', () => {
-            this.map.getView().fit(stac.getExtent());
-        });
-    }
-}
+    stac.on("sourceready", () => {
+      this.map.getView().fit(stac.getExtent());
+    });
+
+    console.log(this.map);
+    console.log("map mounted");
+    console.log(this.map.getLayers().getArray());
+  },
+};
 </script>
-
-<style>
-.container-map {
-
-    width: 100vw;
-    height: 100vh;
-}
-
-#map {
-    width: 100%;
-    height: 100%;
-}
-</style>
