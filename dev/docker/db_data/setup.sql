c01f979bccd9e71c9860fec4f70543ea54bd254e
Vitto50 <vittorio.toffolutti@gmail.com>
2024-04-17 12:16:48 +0200

    initialisation des bdd

--- dev/docker/db_data/setup.sql
+++ dev/docker/db_data/setup.sql
@@ -1,19 +1,24 @@
--- -- INSERTION DE DONNÉES DE TEST POUR LA TABLE Chantier
--- INSERT INTO public.Chantier (id_style, code, nbr_image, stac_url) VALUES
--- (1, 1234, 5, 'https://exemple.com/chantier1'),
--- (2, 5678, 3, 'https://exemple.com/chantier2');
+-- INSERTION DE DONNÉES DE TEST POUR LA TABLE user
+INSERT INTO public.user (id,username,password) VALUES
+(1,'lem','password')
+(2,'leh','1234')
 
--- -- INSERTION DE DONNÉES DE TEST POUR LA TABLE Image_sortie
--- INSERT INTO public.image_sortie (name, , id_chantier) VALUES
--- ('Image1', '{"key": "value"}', 1),
--- ('Image2', '{"key": "value"}', 1),
--- ('Image3', '{"key": "value"}', 2);
+-- INSERTION DE DONNÉES DE TEST POUR LA TABLE Chantier
+INSERT INTO public.Chantier (id_style, code, nbr_image, stac_url, user_key, name) VALUES
+(1, 1234, 5, 'https://exemple.com/chantier1',1,'région de Yzernay'),
+(2, 1234, 5, 'https://exemple.com/chantier1',1,'région Cherbourg'),
+(2, 5678, 3, 'https://exemple.com/chantier2',2,'Région parisienne');
 
--- -- INSERTION DE DONNÉES DE TEST POUR LA TABLE Patch
--- INSERT INTO public.patch (name, id_img_sortie, data) VALUES
--- ('Patch1', 1, '{"key": "value"}'),
--- ('Patch2', 2, '{"key": "value"}'),
--- ('Patch3', 3, '{"key": "value"}');
+-- INSERTION DE DONNÉES DE TEST POUR LA TABLE Image_sortie
+INSERT INTO public.image_sortie (name, id_chantier) VALUES
+('https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/10/T/ES/2022/7/S2A_10TES_20220726_0_L2A/TCI.tif', 1),
+('https://storage.googleapis.com/open-cogs/stac-examples/20201211_223832_CS2.tif', 1);
+
+-- INSERTION DE DONNÉES DE TEST POUR LA TABLE Patch
+INSERT INTO public.patch (name, id_img_sortie) VALUES
+('Patch1', 1),
+('Patch2', 2),
+('Patch3', 3);
 
 -- INSERTION DE DONNÉES DE TEST POUR LA TABLE Catalogue 
 INSERT INTO public.catalogue (name, data) VALUES
