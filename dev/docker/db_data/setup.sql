76d6b1ae471f233b5ae0b0cf966074f13ad501f6
Hannick <61098254+Hannick5@users.noreply.github.com>
2024-04-17 11:12:33 +0200

    update to db_data sql files

--- dev/docker/db_data/setup.sql
+++ dev/docker/db_data/setup.sql
@@ -1,26 +1,26 @@
 -- INSERTION DE DONNÉES DE TEST POUR LA TABLE Chantier
-INSERT INTO public.Chantier (ID_style, Code, Nbr_image, STAC_URL) VALUES
+INSERT INTO public.Chantier (id_style, code, nbr_image, stac_url) VALUES
 (1, 1234, 5, 'https://exemple.com/chantier1'),
 (2, 5678, 3, 'https://exemple.com/chantier2');
 
 -- INSERTION DE DONNÉES DE TEST POUR LA TABLE Image_sortie
-INSERT INTO public.Image_sortie (Name, Data, ID_chantier) VALUES
+INSERT INTO public.image_sortie (name, data, id_chantier) VALUES
 ('Image1', '{"key": "value"}', 1),
 ('Image2', '{"key": "value"}', 1),
 ('Image3', '{"key": "value"}', 2);
 
 -- INSERTION DE DONNÉES DE TEST POUR LA TABLE Patch
-INSERT INTO public.Patch (Name, ID_img_sortie, Data) VALUES
+INSERT INTO public.patch (name, id_img_sortie, data) VALUES
 ('Patch1', 1, '{"key": "value"}'),
 ('Patch2', 2, '{"key": "value"}'),
 ('Patch3', 3, '{"key": "value"}');
 
 -- INSERTION DE DONNÉES DE TEST POUR LA TABLE Catalogue 
-INSERT INTO public.Catalogue (Name, Data) VALUES
+INSERT INTO public.catalogue (name, data) VALUES
 ('Catalogue1', '{"key": "value"}'),
 ('Catalogue2', '{"key": "value"}');
 
 -- INSERTION DE DONNÉES DE TEST POUR LA TABLE COG
-INSERT INTO public.COG (Name, Data, ID_catalogue) VALUES
+INSERT INTO public.cog (name, data, id_catalogue) VALUES
 ('COG1', '{"key": "value"}', 1),
 ('COG2', '{"key": "value"}', 2);
