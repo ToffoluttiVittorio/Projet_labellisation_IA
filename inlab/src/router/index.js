a753a3902398d182ce83579a0b07e52e3def407e
Matheo Marechal <121936719+Matheoia@users.noreply.github.com>
2024-04-18 12:29:43 +0200

    code labellisation plus joli, adapté à Vue

---
+++ inlab/src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import MapView from '../views/MapView.vue'
+import HomeView from '../views/HomeView.vue'
+import LabellisationView from '../views/LabellisationView.vue'
+import PatchView from '../views/PatchView.vue'
+import LoginView from '../views/LoginView.vue'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/map',
+            name: 'map',
+            component: MapView
+        },
+        {
+            path: '/labellisation',
+            name: 'labellisation',
+            component: LabellisationView
+        },
+        {
+            path: '/patch',
+            name: 'patch',
+            component: PatchView
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView
+        }
+    ]
+})
+
+export default router
