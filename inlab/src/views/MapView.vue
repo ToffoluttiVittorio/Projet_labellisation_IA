ecd63459cc6e7c9b6fde18e3e0f6c62de886d087
Matheo Marechal <121936719+Matheoia@users.noreply.github.com>
2024-04-18 09:54:03 +0200

    Add files via upload

---
+++ inlab/src/views/MapView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="MapContainer">
+    <Map />
+  </div>
+</template>
+
+<script>
+import Map from '../components/Map.vue';
+
+export default {
+  components: {
+    Map
+  }
+};
+</script>
+
+<style>
+#MapContainer {
+  position: absolute;
+  top: 5vh;
+  left: 0;
+  width: 100vw;
+  height: 95vh;
+}
+</style>
