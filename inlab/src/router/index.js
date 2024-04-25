194b1640463947c0497585d69e6280072eafebc4
Matheo Marechal <121936719+Matheoia@users.noreply.github.com>
2024-04-25 09:45:24 +0200

    Add files via upload

---
+++ inlab/src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import MapView from "../views/MapView.vue";
+import HomeView from "../views/HomeView.vue";
+import LabellisationView from "../views/LabellisationView.vue";
+import LoginView from "../views/LoginView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/map",
+      name: "map",
+      component: MapView,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView,
+    },
+    {
+      path: "/labellisation/:id",
+      name: "labellisation",
+      component: LabellisationView,
+      props: true,
+    }
+  ],
+});
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = sessionStorage.getItem('username');
+  if (!isAuthenticated && to.path !== '/login') {
+    next('/login');
+  } else {
+    next();
+  }
+});
+
+export default router;
